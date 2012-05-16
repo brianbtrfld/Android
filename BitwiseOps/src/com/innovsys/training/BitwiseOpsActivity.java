5e37d57544c0b73f7b4490fb4a609e6bfa803e46
Brian Butterfield <brian@briangbutterfield.net>
2012-05-16 10:08:37 -0600

    Initial commit

---
+++ BitwiseOps/src/com/innovsys/training/BitwiseOpsActivity.java
@@ -0,0 +1,13 @@
+package com.innovsys.training;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BitwiseOpsActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
