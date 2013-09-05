d548e37efb80c536bf3dbb1a0596ed42f4d9b20f
Brian Butterfield <brian@butter-field.net>
2013-09-05 16:28:08 -0600

    Added logging of state variable with savedInstanceState in onCreate

--- ActivityLifeCycleApp/src/net/briangbutterfield/activitylifecycleapp/Activity1.java
+++ ActivityLifeCycleApp/src/net/briangbutterfield/activitylifecycleapp/Activity1.java
@@ -11,31 +11,47 @@ import android.widget.Button;
 public class Activity1 extends Activity implements OnClickListener
 {
 
-	//NOTES:
-	//  Lifecycle Documentation [http://developer.android.com/reference/android/app/Activity.html#ActivityLifecycle]
-	//  Emulator Rotation [Win = Left Ctrl + F12 OR Mac = fn + Left Ctrl + F12]
-	
+	// NOTES:
+	// Lifecycle Documentation
+	// [http://developer.android.com/reference/android/app/Activity.html#ActivityLifecycle]
+	// Emulator Rotation [Win = Left Ctrl + F12 OR Mac = fn + Left Ctrl + F12]
+
+	private static final String _stateBooleanKey = "StateBoolean";
+
 	private String _message = "";
 	private boolean _stateBoolean = false;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activity1);
-		
+
 		Button buttonStartActivity2 = (Button) findViewById(R.id.buttonStartActivity2);
 		buttonStartActivity2.setOnClickListener(this);
-		
-		_message = this.getString(R.string.activity_one) + " | " + this.getString(R.string.lc_oncreate);
-		Log.d(Common.TAG, _message);
+
+		// Verify whether there was any state saved.
+		if (savedInstanceState == null)
+		{
+			_message = this.getString(R.string.activity_one) + " | " + 
+					   this.getString(R.string.lc_oncreate) +
+						" | StateBoolean = " + String.valueOf(_stateBoolean);
+			Log.d(Common.TAG, _message);
+		}
+		else
+		{
+			_message = this.getString(R.string.activity_one) + " | " +
+					this.getString(R.string.lc_oncreate) +
+					" | StateBoolean = " + String.valueOf(_stateBoolean);
+			Log.d(Common.TAG, _message);
+		}
 	}
 
 	@Override
 	protected void onDestroy()
 	{
 		super.onDestroy();
-		
+
 		_message = this.getString(R.string.activity_one) + " | " + this.getString(R.string.lc_ondestroy);
 		Log.d(Common.TAG, _message);
 	}
@@ -44,9 +60,9 @@ public class Activity1 extends Activity implements OnClickListener
 	protected void onPause()
 	{
 		super.onPause();
-		
+
 		_stateBoolean = true;
-		
+
 		_message = this.getString(R.string.activity_one) + " | " + this.getString(R.string.lc_onpause);
 		Log.d(Common.TAG, _message);
 	}
@@ -55,28 +71,30 @@ public class Activity1 extends Activity implements OnClickListener
 	protected void onSaveInstanceState(Bundle outState)
 	{
 		super.onSaveInstanceState(outState);
-		
-		outState.putBoolean("StateBoolean", _stateBoolean);
-		
-		_message = this.getString(R.string.activity_one) + " | " + 
-		           this.getString(R.string.lc_onsaveinstancestate) + 
-				   " | StateBoolean = " + String.valueOf(_stateBoolean);
-		
+
+		outState.putBoolean(_stateBooleanKey, _stateBoolean);
+
+		_message = this.getString(R.string.activity_one) + " | " +
+				this.getString(R.string.lc_onsaveinstancestate) +
+				" | StateBoolean = " + String.valueOf(_stateBoolean);
+
 		Log.d(Common.TAG, _message);
 	}
 
 	@Override
 	protected void onRestoreInstanceState(Bundle savedInstanceState)
 	{
-		// NOTE:  Only called when Activity is killed, i.e. low memory condition or device rotation.
+		// NOTE 1: Only called when Activity is killed, i.e. device rotation.
+		// NOTE 2: Only called when there is state to be restored.
 		super.onRestoreInstanceState(savedInstanceState);
-		
-		_stateBoolean = savedInstanceState.getBoolean("StateBoolean");
-		
-		_message = this.getString(R.string.activity_one) + " | " + 
-		           this.getString(R.string.lc_onrestoreinstancestate) + 
-				   " | StateBoolean = " + String.valueOf(_stateBoolean);
-		
+
+		// Restore the state member variable.
+		_stateBoolean = savedInstanceState.getBoolean(_stateBooleanKey);
+
+		_message = this.getString(R.string.activity_one) + " | " +
+				this.getString(R.string.lc_onrestoreinstancestate) +
+				" | StateBoolean = " + String.valueOf(_stateBoolean);
+
 		Log.d(Common.TAG, _message);
 	}
 
@@ -84,7 +102,7 @@ public class Activity1 extends Activity implements OnClickListener
 	protected void onResume()
 	{
 		super.onResume();
-		
+
 		_message = this.getString(R.string.activity_one) + " | " + this.getString(R.string.lc_onresume);
 		Log.d(Common.TAG, _message);
 	}
@@ -93,7 +111,7 @@ public class Activity1 extends Activity implements OnClickListener
 	protected void onRestart()
 	{
 		super.onRestart();
-		
+
 		_message = this.getString(R.string.activity_one) + " | " + this.getString(R.string.lc_onrestart);
 		Log.d(Common.TAG, _message);
 	}
@@ -102,7 +120,7 @@ public class Activity1 extends Activity implements OnClickListener
 	protected void onStart()
 	{
 		super.onStart();
-		
+
 		_message = this.getString(R.string.activity_one) + " | " + this.getString(R.string.lc_onstart);
 		Log.d(Common.TAG, _message);
 	}
@@ -111,11 +129,11 @@ public class Activity1 extends Activity implements OnClickListener
 	protected void onStop()
 	{
 		super.onStop();
-		
+
 		_message = this.getString(R.string.activity_one) + " | " + this.getString(R.string.lc_onstop);
 		Log.d(Common.TAG, _message);
 	}
-	
+
 	@Override
 	public void onClick(View v)
 	{
@@ -124,7 +142,7 @@ public class Activity1 extends Activity implements OnClickListener
 
 	private void callActivity2()
 	{
-		// Create an intent to start the activity, notice the use of the 
+		// Create an intent to start the activity, notice the use of the
 		// actual class name.
 		Intent intent = new Intent(this, Activity2.class);
 		startActivity(intent);
